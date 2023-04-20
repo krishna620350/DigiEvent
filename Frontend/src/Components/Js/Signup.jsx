b3e05d7e2ede19fa1f4339ad9c523e27715a1ec2
Inconsis10 <a.63.pratikkumar@gmail.com>
2023-04-21 02:33:14 +0530

    Removed currentUser state as AuthContext file was removed by Krishna

--- Frontend/src/Components/Js/Signup.jsx
+++ Frontend/src/Components/Js/Signup.jsx
@@ -5,12 +5,10 @@ import Menu from './Navbar';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/esm/Container';
 import { useState } from 'react';
-import { useAuth } from '../../context/AuthContext';
 import validateAuthInput from '../../utils/validateAuthInput';
 import FormErrorMessage from './FormErrorMessage';
 
 function Signup() {
-  const {currentUser} =useAuth();
   // console.log(currentUser);
   const [userInfo,setUserInfo]=useState({
     name:"",
